6f121d8b4220b7a1f74dd32eb57a7372283e33c2
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2022-04-26 23:15:14 +0900

    [#section2] 사용자가 입력한 값을 메뉴 리스트에 추가하기 | DOM 핸들링

--- src/js/index.js
+++ src/js/index.js
@@ -7,19 +7,41 @@
 // - [ ] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
 // - [ ] 사용자 입력값이 빈 값이라면 추가되지 않는다.
 
+
+const $ = (selector) => document.querySelector(selector);
+
 function App() {
     // form 태그가 전송되는걸 막아준다.
-    document.querySelector("#espresso-menu-form").addEventListener("submit", (e) => {
+    $("#espresso-menu-form").addEventListener("submit", (e) => {
         e.preventDefault();
     })
     // 메뉴의 이름을 입력받는건
-    document
-        .querySelector('#espresso-menu-name')
-        .addEventListener("keypress", (e) => {
-            if (e.key == "Enter") {
-                console.log(document.querySelector('#espresso-menu-name').value);
-            }
-        });
+    $('#espresso-menu-name').addEventListener("keypress", (e) => {
+        if (e.key == "Enter") {
+            const espressMenuName = $('#espresso-menu-name').value;
+            const menuItemTemplate = (espressMenuName) => {
+                return `<li class="menu-list-item d-flex items-center py-2">
+                <span class="w-100 pl-2 menu-name">${espressMenuName}</span>
+                <button
+                  type="button"
+                  class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+                >
+                  수정
+                </button>
+                <button
+                  type="button"
+                  class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+                >
+                  삭제
+                </button>
+              </li>`;
+            };
+            // innerHTML
+            // $('#espresso-menu-list').innerHTML = (menuItemTemplate(espressMenuName));
+            // insertAdjacentHTML
+            $('#espresso-menu-list').insertAdjacentHTML("beforeend", menuItemTemplate(espressMenuName));
+        }
+    });
 }
 
 App();
