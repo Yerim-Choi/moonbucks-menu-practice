827173660e57655800226d3ebe4983d799296a89
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2022-04-28 00:09:34 +0900

    [#section2] 빈 값 입력에 대한 대응 | 예외 처리

--- src/js/index.js
+++ src/js/index.js
@@ -4,8 +4,8 @@
 // - [x] 메뉴의 이름을 입력 받고 엔터키 입력으로 추가한다.
 // - [x] 추가되는 메뉴의 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
 // - [x] 총 메뉴 갯수를 count하여 상단에 보여준다.
-// - [ ] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
-// - [ ] 사용자 입력값이 빈 값이라면 추가되지 않는다.
+// - [x] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
+// - [x] 사용자 입력값이 빈 값이라면 추가되지 않는다.
 
 
 const $ = (selector) => document.querySelector(selector);
@@ -17,10 +17,19 @@ function App() {
   })
   // 메뉴의 이름을 입력받는건
   $('#espresso-menu-name').addEventListener("keypress", (e) => {
-    if (e.key == "Enter") {
-      const espressMenuName = $('#espresso-menu-name').value;
-      const menuItemTemplate = (espressMenuName) => {
-        return `<li class="menu-list-item d-flex items-center py-2">
+    // 사용자가 처음 입력한 값이 Enter가 아닌 경우
+    if (e.key !== "Enter") {
+      return;
+    }
+    // 입력한 데이터가 빈 값일 경우
+    if ($('#espresso-menu-name').value === '') {
+      alert('값을 입력해주세요.');
+      return;
+    }
+
+    const espressMenuName = $('#espresso-menu-name').value;
+    const menuItemTemplate = (espressMenuName) => {
+      return `<li class="menu-list-item d-flex items-center py-2">
                 <span class="w-100 pl-2 menu-name">${espressMenuName}</span>
                 <button
                   type="button"
@@ -35,16 +44,19 @@ function App() {
                   삭제
                 </button>
               </li>`;
-      };
-      // innerHTML
-      // $('#espresso-menu-list').innerHTML = (menuItemTemplate(espressMenuName));
-      // insertAdjacentHTML
-      $('#espresso-menu-list').insertAdjacentHTML("beforeend", menuItemTemplate(espressMenuName));
+    };
+    // innerHTML
+    // $('#espresso-menu-list').innerHTML = (menuItemTemplate(espressMenuName));
+    // insertAdjacentHTML
+    $('#espresso-menu-list').insertAdjacentHTML("beforeend", menuItemTemplate(espressMenuName));
 
-      // innerText
-      const menuCount = $('#espresso-menu-list').querySelectorAll('li').length;
-      $('.menu-count').innerText = `총 ${menuCount}개`
-    }
+    // innerText
+    const menuCount = $('#espresso-menu-list').querySelectorAll('li').length;
+    $('.menu-count').innerText = `총 ${menuCount}개`
+
+    // 추가 후 input 초기화
+    $('#espresso-menu-name').value = '';
+
   });
 }
 
