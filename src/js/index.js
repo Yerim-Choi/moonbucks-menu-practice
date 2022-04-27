d57b3a2e494211453c41c86a0042dff4aebfe1bc
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2022-04-27 15:04:44 +0900

    [#section2] 카운트 갯수 업데이트 | DOM 핸들링

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
 
 // step1 요구사항 구현을 위한 전략
 // TODO 메뉴 추가
-// - [ ] 메뉴의 이름을 입력 받고 엔터키 입력으로 추가한다.
-// - [ ] 추가되는 메뉴의 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
-// - [ ] 총 메뉴 갯수를 count하여 상단에 보여준다.
+// - [x] 메뉴의 이름을 입력 받고 엔터키 입력으로 추가한다.
+// - [x] 추가되는 메뉴의 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
+// - [x] 총 메뉴 갯수를 count하여 상단에 보여준다.
 // - [ ] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
 // - [ ] 사용자 입력값이 빈 값이라면 추가되지 않는다.
 
@@ -11,16 +11,16 @@
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
-    // form 태그가 전송되는걸 막아준다.
-    $("#espresso-menu-form").addEventListener("submit", (e) => {
-        e.preventDefault();
-    })
-    // 메뉴의 이름을 입력받는건
-    $('#espresso-menu-name').addEventListener("keypress", (e) => {
-        if (e.key == "Enter") {
-            const espressMenuName = $('#espresso-menu-name').value;
-            const menuItemTemplate = (espressMenuName) => {
-                return `<li class="menu-list-item d-flex items-center py-2">
+  // form 태그가 전송되는걸 막아준다.
+  $("#espresso-menu-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+  })
+  // 메뉴의 이름을 입력받는건
+  $('#espresso-menu-name').addEventListener("keypress", (e) => {
+    if (e.key == "Enter") {
+      const espressMenuName = $('#espresso-menu-name').value;
+      const menuItemTemplate = (espressMenuName) => {
+        return `<li class="menu-list-item d-flex items-center py-2">
                 <span class="w-100 pl-2 menu-name">${espressMenuName}</span>
                 <button
                   type="button"
@@ -35,13 +35,17 @@ function App() {
                   삭제
                 </button>
               </li>`;
-            };
-            // innerHTML
-            // $('#espresso-menu-list').innerHTML = (menuItemTemplate(espressMenuName));
-            // insertAdjacentHTML
-            $('#espresso-menu-list').insertAdjacentHTML("beforeend", menuItemTemplate(espressMenuName));
-        }
-    });
+      };
+      // innerHTML
+      // $('#espresso-menu-list').innerHTML = (menuItemTemplate(espressMenuName));
+      // insertAdjacentHTML
+      $('#espresso-menu-list').insertAdjacentHTML("beforeend", menuItemTemplate(espressMenuName));
+
+      // innerText
+      const menuCount = $('#espresso-menu-list').querySelectorAll('li').length;
+      $('.menu-count').innerText = `총 ${menuCount}개`
+    }
+  });
 }
 
 App();
