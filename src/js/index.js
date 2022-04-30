7ed2b5d0368ac67ad4d8e9e7b72962f213ae773d
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2022-04-30 23:59:05 +0900

    [#section2] 수정 버튼을 눌러 'prompt'창에서 메뉴 수정 | 브라우저 인터페이스

--- src/js/index.js
+++ src/js/index.js
@@ -13,6 +13,24 @@ const $ = (selector) => document.querySelector(selector);
 
 function App() {
 
+  // TODO 메뉴 수정
+  // - [x] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달창(prompt)이 뜬다.
+  // - [x] 모달창에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
+
+  // 메뉴 수정
+  $('#espresso-menu-list').addEventListener("click", (e) => {
+    if (e.target.classList.contains('menu-edit-button')) {
+      const $menuName = e.target.closest('li').querySelector('.menu-name');
+      const updatedMenuName = prompt(
+        '메뉴명을 수정하세요',
+        $menuName.innerText
+      );
+      $menuName.innerText = updatedMenuName;
+    }
+  })
+
+
+
   $("#espresso-menu-form").addEventListener("submit", (e) => {
     e.preventDefault();   // form 태그가 전송되는걸 막아준다.
   })
@@ -73,9 +91,7 @@ function App() {
 App();
 
 
-// TODO 메뉴 수정
-// - [ ] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달창이 뜬다.
-// - [ ] 모달창에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
+
 
 // TODO 메뉴 삭제
 // - [ ] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다.
