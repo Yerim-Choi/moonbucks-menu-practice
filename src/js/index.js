a4d9bcc471017a8ddddba7fed8be4a22787225ba
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2022-05-14 23:09:07 +0900

    [#section3] 품절 상태 관리

--- src/js/index.js
+++ src/js/index.js
@@ -42,10 +42,10 @@
 // - [x] 에스프레소 메뉴를 페이지에 그려준다.
 
 // TODO 품절 상태 관리
-// - [ ] 품절 상태인 경우를 보여줄 수 있게, 품절 버튼을 추가하고 sold-out class를 추가하여 상태를 변경한다.
-// - [ ] 품절 버튼을 추가한다.
-// - [ ] 품절 버튼을 클릭하면 localStorage에 상태값이 저장된다.
-// - [ ] 클릭 이벤트에서 가장 가까운 <li> 태그의 class 속성 값에 sold-out 을 추가한다.
+// - [x] 품절 상태인 경우를 보여줄 수 있게, 품절 버튼을 추가하고 sold-out class를 추가하여 상태를 변경한다.
+// - [x] 품절 버튼을 추가한다.
+// - [x] 품절 버튼을 클릭하면 localStorage에 상태값이 저장된다.
+// - [x] 클릭 이벤트에서 가장 가까운 <li> 태그의 class 속성 값에 sold-out 을 추가한다.
 
 
 const $ = (selector) => document.querySelector(selector);
@@ -81,7 +81,13 @@ function App() {
 
     const template = this.menu[this.currentCategory].map((menuItem, index) => {
       return `<li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
-      <span class="w-100 pl-2 menu-name">${menuItem.name}</span>
+      <span class="w-100 pl-2 menu-name ${menuItem.soldOut ? "sold-out" : ""}">${menuItem.name}</span>
+      <button
+      type="button"
+      class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button"
+      >
+        품절
+      </button>
       <button
         type="button"
         class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
@@ -145,13 +151,27 @@ function App() {
     }
   }
 
+  const soldOutMenu = (e) => {
+    const menuId = e.target.closest('li').dataset.menuId;
+    this.menu[this.currentCategory][menuId].soldOut = !this.menu[this.currentCategory][menuId].soldOut;
+    store.setLocalStorage(this.menu);
+    render();
+  }
+
   $('#menu-list').addEventListener("click", (e) => {
     if (e.target.classList.contains('menu-edit-button')) {
       updateMenuName(e);
+      return;
     }
 
     if (e.target.classList.contains("menu-remove-button")) {
       removeMenuName(e);
+      return;
+    }
+
+    if (e.target.classList.contains("menu-sold-out-button")) {
+      soldOutMenu(e);
+      return;
     }
   });
 
